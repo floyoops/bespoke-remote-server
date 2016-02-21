283b6f401bcbb1913556fde439bce00cd8c9448a
Florian Chaudret <florian.chaudret@gmail.com>
2016-02-21 02:19:34 +0100

    Config capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'BespokeRemoteServer'
+set :repo_url, 'https://github.com/floyoops/bespoke-remote-server.git'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/deploy/bespoke_project/bespoke-remote-server'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 2
+
+namespace :deploy do
+
+  after :updated, :npm do
+    invoke "npm:install"
+  end
+
+  after :publishing, :supervisor do
+  	invoke "supervisor:restart"
+  end
+
+end
